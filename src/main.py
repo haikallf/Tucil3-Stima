f003676926e2cb46fb4a70c7dfd28f47841cdca7
harithfakhiri <68526483+harithfakhiri@users.noreply.github.com>
2021-04-06 03:29:54 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from astar import bacaTextFile, listOfNode, tetangga, coordinates, jarak, matrixJarakTetangga, getNeighbour, getIdx, sortingFn, closestPath, getMinFn, derivate
+from astar import bacaTextFile, listOfNode, tetangga, coordinates, jarak, matrixJarak, getNeighbour, getIdx, sortingFn, closestPath, getMinFn, derivate
               
 filename = str(input("Masukkan nama file : "))
 fileDir = "../test/" + filename
@@ -6,11 +6,11 @@ arr = bacaTextFile(fileDir)
 listNode = listOfNode(arr) #['A', 'B', 'C', 'D', ...] A,B,C,D,... adalah node 
 arrNeigh = tetangga(arr,listNode) # [('A', 'B'), ('A', 'C'), ...] B tetangga A, C tetangga A
 loc = coordinates(arr) # [['1', '2'], ['2', '1'], ['1','1'], ...] elmt pertama adalah ada latitude, dan kedua adalah longitude
-matrixDistTetangga = matrixJarakTetangga(arr, loc)
+matrixDistance = matrixJarak(arr, loc)
 # 0 2 1
 # 2 0 2
 # 1 2 0
 src = str(input("Masukkan node awal : ")) # A
 dest = str(input("Masukkan node tujuan : ")) # G
-jalan = closestPath(src, dest, arrNeigh, matrixDistTetangga, listNode) # [('A', 0), ('B', 20.62), ('G', 159.84)], elmt kedua dari tuple dalam satuan meter
+jalan = closestPath(src, dest, arrNeigh, matrixDistance, listNode) # [('A', 0), ('B', 20.62), ('G', 159.84)], elmt kedua dari tuple dalam satuan meter
 print(jalan)
